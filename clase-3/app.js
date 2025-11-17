70c6072db15dfd604ee97d21ea76589fe5b22277
xaxo <jbaigorri@hotmail.com>
2025-11-17 13:54:36 +0100

    Borrando una pelicula sin usar require('cors') aun

--- clase-3/app.js
+++ clase-3/app.js
@@ -1,10 +1,116 @@
 const express = require('express')
+const crypto = require('node:crypto')
+// const cors = require('cors')
+const movies = require('./movies.json')
+const { validateMovie, validatePartialMovie } = require('./schemas/movies')
 
+/// voy por el minuto 31 con 50 segundos
+/// https://www.youtube.com/watch?v=-9d3KhCqOtU&list=PLUofhDIg_38qm2oPOV-IRTTEKyrVBBaU7&index=4
 const app = express()
+app.use(express.json())
 app.disable('x-powered-by') // deshabilitar el header X-Power-By: Express
 
-app.length('/', (req, res) => {
-  res.json({ message: 'hola mundo' })
+const ACCEPTED_ORIGINS = [
+  'http://localhost:8080',
+  'http://localhost:3000',
+  'https://movies.com'
+]
+
+// Todos los recursos que sean MOVIES se identica con /movies
+app.get('/movies', (req, res) => {
+  const origin = req.header('origin')
+
+  if (ACCEPTED_ORIGINS.includes(origin) || !origin) {
+    res.header('Access-Control-Allow-Origin', origin)
+  }
+
+  const { genre } = req.query
+  if (genre) {
+    const filteredMovies = movies.filter(
+      movie => movie.genre.some(g => g.toLocaleLowerCase() === genre.toLocaleLowerCase())
+    )
+    return res.json(filteredMovies)
+  }
+  res.json(movies)
+})
+
+app.delete('/movies/:id', (req, res) => {
+  const origin = req.header('origin')
+
+  if (ACCEPTED_ORIGINS.includes(origin) || !origin) {
+    res.header('Access-Control-Allow-Origin', origin)
+  }
+
+  const { id } = req.params
+  const movieIndex = movies.findIndex(movie => movie.id === id)
+
+  if (movieIndex < 0) {
+    return res.status(404).json({ message: 'Movie not found' })
+  }
+
+  movies.splice(movieIndex, 1)
+  return res.status(301).json({ message: 'Movie deleted' })
+})
+
+// Para recuperar una película por ID - Segmento dinámico
+app.get('/movies/:id', (req, res) => { // path-to-regexp
+  const { id } = req.params
+  const movie = movies.find(movie => movie.id === id)
+  if (movie) return res.json(movie)
+  res.status(404).json({ message: 'Movie not found' })
+})
+
+app.post('/movies', (req, res) => {
+  // eslint-disable-next-line no-undef
+  const result = validateMovie(req.body)
+
+  if (result.error) {
+    // 422 Unprocessabe Entity
+    return res.status(400).json({ error: JSON.parse(result.error.message) })
+  }
+
+  const newMovie = {
+    id: crypto.randomUUID(), // uuid v4
+    ...result.data
+  }
+
+  // Esto no sería REST, porque estmos guardando
+  // el estdo de la aplicación en memoria
+  movies.push(newMovie)
+
+  res.status(201).json(newMovie) // actualizar la caché del cliente
+})
+
+app.patch('/movies/:id', (req, res) => {
+  const result = validatePartialMovie(req.body)
+
+  if (!result.success) {
+    return res.status(400).json({ error: JSON.parse(result.error.message) })
+  }
+
+  const { id } = req.params
+  const movieIndex = movies.findIndex(movie => movie.id === id)
+
+  if (movieIndex < 0) return res.status(404).json({ message: 'Movie not found' })
+
+  const updateMovie = {
+    ...movies[movieIndex],
+    ...result.data
+  }
+
+  movies[movieIndex] = updateMovie
+
+  return res.json(updateMovie)
+})
+
+app.options('/movies/:id', (req, res) => {
+  const origin = req.header('origin')
+
+  if (ACCEPTED_ORIGINS.includes(origin) || !origin) {
+    res.header('Access-Control-Allow-Origin', origin)
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE')
+  }
+  res.send(200)
 })
 
 // escribir en consola.
